6ba05856983f10d40d38bcd56f336bc7a539afbb
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2025-04-30 18:45:40 -0600

    pulled updated src

---
+++ src/main/java/com/example/app/controllers/ViewDataController.java
@@ -0,0 +1,286 @@
+package com.example.app.controllers;
+
+import com.example.app.dao.BuildingRecords;
+import com.example.app.dao.DBConn;
+import com.example.app.dao.GasRecords;
+import com.example.app.dao.UtilityRecords;
+import com.example.app.model.Building;
+import com.example.app.utils.FilteredBuildingBox;
+import com.example.app.model.Gas;
+import com.example.app.model.Utility;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ViewDataController{
+    private DBConn dbConn;
+    private BuildingRecords buildingRecords;
+    private ArrayList<Building> buildings;
+    private UtilityRecords utilityRecords;
+    private GasRecords gasRecords;
+    private FilteredBuildingBox filteredBuildingBox;
+
+    private Utility utility = new Utility();
+
+    // Left Side Panel
+    @FXML private ComboBox<Building> buildingComboBox;
+    @FXML private DatePicker startDatePicker;
+    @FXML private DatePicker endDatePicker;
+    @FXML private CheckBox electricityUsageCheck;
+    @FXML private CheckBox waterUsageCheck;
+    @FXML private CheckBox electricityCostCheck;
+    @FXML private CheckBox waterCostCheck;
+    @FXML private CheckBox sewageCostCheck;
+    @FXML private CheckBox miscCostCheck;
+
+    // Center Panel
+    // Table View
+    @FXML private SplitPane mainSplitPane;
+    @FXML private LineChart<String, Number> lineChart;
+    @FXML private BarChart<String, Number> barChart;
+    @FXML private PieChart pieChart;
+    @FXML private TabPane tabPane;
+    @FXML private Tab utilityTab;
+    @FXML private Tab gasTab;
+    @FXML private TableView<Utility> utilityTableView;
+    @FXML private TableView<Gas> gasTableView;
+
+    @FXML private TableColumn<Utility, String> dateColumn;
+    @FXML private TableColumn<Utility, Float> electricityUsageColumn;
+    @FXML private TableColumn<Utility, Float> waterUsageColumn;
+    @FXML private TableColumn<Utility, Float> electricityCostColumn;
+    @FXML private TableColumn<Utility, Float> waterCostColumn;
+    @FXML private TableColumn<Utility, Float> sewageCostColumn;
+    @FXML private TableColumn<Utility, Float> miscCostColumn;
+
+    @FXML private TableColumn<Gas, String> rateColumn;
+    @FXML private TableColumn<Gas, Float> currentChargeColumn;
+    @FXML private TableColumn<Gas, String> fromBillingColumn;
+    @FXML private TableColumn<Gas, String> toBillingColumn;
+    @FXML private TableColumn<Gas, Float> meterReadColumn;
+    @FXML private TableColumn<Gas, Float> billedCCFColumn;
+
+
+    public ViewDataController(){}
+
+    public ViewDataController(DBConn conn){
+        dbConn = conn;
+    }
+
+    public void initialize() {
+        if (dbConn == null) {
+            System.out.println("No active database connection.");
+            return;
+        }
+
+        buildingRecords = new BuildingRecords(dbConn);
+        buildings = buildingRecords.getBuildings();
+        filteredBuildingBox = new FilteredBuildingBox(buildings, buildingComboBox);
+
+        utilityRecords = new UtilityRecords(dbConn);
+        gasRecords = new GasRecords(dbConn);
+
+        // Disable manual typing in datePicker
+        startDatePicker.getEditor().setDisable(true);
+        startDatePicker.getEditor().setOpacity(1);
+        endDatePicker.getEditor().setDisable(true);
+        endDatePicker.getEditor().setOpacity(1);
+
+
+        // https://docs.oracle.com/javase/8/javafx/api/javafx/beans/property/SimpleFloatProperty.html#SimpleFloatProperty-java.lang.Object-java.lang.String-
+        // Properties can be bound in ways that will automatically update the UI when something changes
+        dateColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDate().toString()));
+        electricityUsageColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getElectricityUsage()).asObject());
+        waterUsageColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getWaterUsage()).asObject());
+        electricityCostColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getElectricityCost()).asObject());
+        waterCostColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getWaterCost()).asObject());
+        sewageCostColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getSewageCost()).asObject());
+        miscCostColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getMiscCost()).asObject());
+
+        fromBillingColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getFromBilling().toString()));
+        toBillingColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getToBilling().toString()));
+        rateColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getRate()));
+        currentChargeColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getCurrentCharges()).asObject());
+        meterReadColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getMeterRead()).asObject());
+        billedCCFColumn.setCellValueFactory(data -> new SimpleFloatProperty(data.getValue().getBilledCCF()).asObject());
+
+
+    }
+
+    @FXML public void refreshView(ActionEvent event) {
+        Building building = buildingComboBox.getValue();
+        if (building == null) {
+            // We need to create a view that displays errors, similar to the database view
+            System.out.println("Building Not Selected.");
+            return;
+        }
+
+        LocalDate startDate = startDatePicker.getValue();
+        LocalDate endDate = endDatePicker.getValue();
+        if (startDate == null) {
+            // send error to user
+            System.out.println("Start Date not selected.");
+            return;
+        } else if (endDate == null) {
+            // send error to user
+            System.out.println("End date not selected.");
+            return;
+        }
+
+        boolean showElectricityUsage = electricityUsageCheck.isSelected();
+        boolean showWaterUsage = waterUsageCheck.isSelected();
+        boolean showElectricityCost = electricityCostCheck.isSelected();
+        boolean showWaterCost = waterCostCheck.isSelected();
+        boolean showSewageCost = sewageCostCheck.isSelected();
+        boolean showMiscCost = miscCostCheck.isSelected();
+
+        ArrayList<Utility> utilities = utilityRecords.getUtilities(building.getBuildingID(), startDate, endDate, dbConn);
+
+        populateCharts(utilities, showElectricityUsage, showWaterUsage, showElectricityCost,
+                        showWaterCost, showSewageCost, showMiscCost);
+
+        ArrayList<Gas> gasList = gasRecords.getGas(building.getBuildingID(), startDate, endDate, dbConn);
+
+        populateTables(utilities, gasList);
+    }
+
+    private void populateTables(ArrayList<Utility> utilities, ArrayList<Gas> gasList) {
+        // Utility
+        utilityTableView.getItems().clear();
+
+        ObservableList<Utility> oUtilities = FXCollections.observableArrayList(utilities);
+        utilityTableView.setItems(oUtilities);
+
+        // Gas
+        gasTableView.getItems().clear();
+
+        ObservableList<Gas> oGas = FXCollections.observableArrayList(gasList);
+        gasTableView.setItems(oGas);
+    }
+
+    private void populateCharts(ArrayList<Utility> utilities, boolean showElectricityUsage, boolean showWaterUsage, boolean showElectricityCost,
+                                boolean showWaterCost, boolean showSewageCost, boolean showMiscCost) {
+        // LineChart
+        lineChart.getData().clear();
+        XYChart.Series<String, Number> electricityUsage = null;
+        XYChart.Series<String, Number> waterUsage = null;
+        XYChart.Series<String, Number> electricityCost = null;
+        XYChart.Series<String, Number> waterCost = null;
+        XYChart.Series<String, Number> sewageCost = null;
+        XYChart.Series<String, Number> miscCost = null;
+
+        if (electricityUsageCheck.isSelected()) {
+            electricityUsage = new XYChart.Series<>();
+            electricityUsage.setName("Electricity Usage");
+        }
+
+        if (waterUsageCheck.isSelected()) {
+            waterUsage = new XYChart.Series<>();
+            waterUsage.setName("Water Usage");
+        }
+
+        if (electricityCostCheck.isSelected()) {
+            electricityCost = new XYChart.Series<>();
+            electricityCost.setName("Electricity Cost");
+        }
+
+        if (waterCostCheck.isSelected()) {
+            waterCost = new XYChart.Series<>();
+            waterCost.setName("Water Cost");
+        }
+
+        if (sewageCostCheck.isSelected()) {
+            sewageCost = new XYChart.Series<>();
+            sewageCost.setName("Sewage Cost");
+        }
+
+        if (miscCostCheck.isSelected()) {
+            miscCost = new XYChart.Series<>();
+            miscCost.setName("Misc Cost");
+        }
+
+        for (Utility utility : utilities) {
+            String dateToString = utility.getDate().toString();
+            if (electricityUsage != null) {
+                electricityUsage.getData().add(new XYChart.Data<>(dateToString, utility.getElectricityUsage()));
+            }
+            if (waterUsage != null) {
+                waterUsage.getData().add(new XYChart.Data<>(dateToString, utility.getWaterUsage()));
+            }
+            if (electricityCost != null) {
+                electricityCost.getData().add(new XYChart.Data<>(dateToString, utility.getElectricityCost()));
+            }
+            if (waterCost != null) {
+                waterCost.getData().add(new XYChart.Data<>(dateToString, utility.getWaterCost()));
+            }
+            if (sewageCost != null) {
+                sewageCost.getData().add(new XYChart.Data<>(dateToString, utility.getSewageCost()));
+            }
+            if (miscCost != null) {
+                miscCost.getData().add(new XYChart.Data<>(dateToString, utility.getMiscCost()));
+            }
+        }
+
+        /*
+        Was trying to ignore the NULL values in the database but when the following if statements are executed
+        with the types in the Utility model set to Float so they can capture NULL, nothing is populated in the
+        bar chart for some reason.
+         */
+
+//        if (electricityUsage != null) {
+//            Float value = utility.getElectricityUsage();
+//            if (value != null) {
+//                lineChart.getData().add(electricityUsage);
+//            }
+//        }
+// This is currently only checking if the last utility in the list. Previous utilities with values are skipped leading
+// to the graph being empty in the majority of situations.
+        if (electricityUsage != null && !electricityUsage.getData().isEmpty()) {
+            lineChart.getData().add(electricityUsage);
+        }
+        if (waterUsage != null && !waterUsage.getData().isEmpty()) {
+            lineChart.getData().add(waterUsage);
+        }
+        if (electricityCost != null && !electricityCost.getData().isEmpty()) {
+            lineChart.getData().add(electricityCost);
+        }
+        if (waterCost != null && !waterCost.getData().isEmpty()) {
+            lineChart.getData().add(waterCost);
+        }
+        if (sewageCost != null && !sewageCost.getData().isEmpty()) {
+            lineChart.getData().add(sewageCost);
+        }
+        if (miscCost != null && !miscCost.getData().isEmpty()) {
+            lineChart.getData().add(miscCost);
+        }
+
+        // Bar Chart
+        barChart.getData().clear();
+
+        Map<String, Float> usageDictionary = utilityRecords.getBuildingTotalUsage();
+
+        XYChart.Series<String, Number> usageByBuilding = new XYChart.Series<>();
+        usageByBuilding.setName("Total Usage");
+
+        for (Map.Entry<String, Float> entry : usageDictionary.entrySet()) {
+            XYChart.Data<String, Number> data = new XYChart.Data<>(entry.getKey(), entry.getValue());
+            usageByBuilding.getData().add(data);
+        }
+
+        barChart.getData().add(usageByBuilding);
+    }
+}
---
+++ src/main/java/com/example/app/dao/DBQueries.java
@@ -0,0 +1,133 @@
+package com.example.app.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// Interface to call
+// Will have Basic CRUD operations, but will provide with override for custom classes
+public interface DBQueries {
+    default boolean insert(String table, String columns, String values, DBConn dbConn) {
+        Connection connection = dbConn.getConnection();
+        if (connection == null) {
+            System.out.println("No active database connection.");
+            return false;
+        }
+
+        String query = "INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ")";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /* Example Use of insert
+        DBController dbController = new DBController();
+        DBQueries dbQueries = new DBQueries() {}; // Anonymous implementation
+        boolean success = dbQueries.insert("users", "name, email", "'John Doe', 'john@example.com'", dbController);
+    * */
+
+    // Need to add Read / Update / Delete
+    default ResultSet read(String table, String columns, String condition, DBConn dbConn) throws SQLException {
+        Connection connection = dbConn.getConnection();
+        if (connection == null) {
+
+            System.out.println("No active database connection.");
+            return null;
+        }
+
+        String query = String.format("SELECT %s FROM %s", columns, table);
+        if (!condition.isEmpty()) {
+            condition = String.format(" WHERE %s", condition);
+            query += condition;
+        }
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            return statement.executeQuery();
+        } catch (SQLException e) {
+            System.out.printf("Caught SQL Error: %s", e);
+            return null;
+        }
+    }
+
+    default boolean update(String table, String setClause, String condition, DBConn dbConn) {
+        Connection connection = dbConn.getConnection();
+        if (connection == null) {
+            System.out.println("No active db Connection.");
+            return false;
+        }
+        String query = String.format("UPDATE %s SET %s", table, setClause);
+        if (!condition.isEmpty()) {
+            query += String.format(" WHERE %s", condition);
+        }
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            int rowsAffected = statement.executeUpdate();
+            System.out.println("Rows updated: " + rowsAffected);
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.printf("Caught SQL Error in update: %s%n", e);
+            return false;
+        }
+    }
+    /* Example Use of default update
+     *  DBController dbController = new DBController();
+     *  DBQueries dbQueries = new DBQueries() {};
+     *
+     *  boolean updated = dbQueries.update(
+     *      "users",                                   // table
+     *      "name='Jane Doe', email='jane@site.com'",  // SET clause
+     *      "id = 1",                                  // WHERE clause
+     *      dbController                               // connection provider
+     *  );
+     *  // This will build a SQL statement that would be ran as such
+     *  // UPDATE users SET name='Jane Doe', email='jane@site.com' WHERE id = 1;
+     *
+     *  // Check if update was successful
+     *  if (updated) { System.out.println("User updated successfully."); }
+     *  else { System.out.println("No user was updated."); }
+     * */
+
+    default boolean delete(String table, String condition, DBConn dbConn) {
+        Connection connection = dbConn.getConnection();
+        if (connection == null) {
+            System.out.println("No active db Connection.");
+            return false;
+        }
+
+        // Safety to prevent full table wipe
+        if (condition == null || condition.isEmpty()) {
+            System.out.println("Delete condition is required to avoid removing all records!");
+            return false;
+        }
+
+        String query = String.format("DELETE FROM %s WHERE %s", table, condition);
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            int rowsAffected = statement.executeUpdate();
+            System.out.println("Rows deleted: " + rowsAffected);
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            System.out.printf("Caught SQL Error in delete: %s%n", e);
+            return false;
+        }
+    }
+    /* Example Use of default delete
+     *  DBController dbController = new DBController();
+     *  DBQueries dbQueries = new DBQueries() {}; // anonymous implementation
+     *
+     *  boolean deleted = dbQueries.delete(
+     *      "users",          // table
+     *      "id = 1",         // WHERE clause
+     *      dbController      // connection provider
+     *  );
+     *
+     *  // This will build and execute a SQL statement like:
+     *  // DELETE FROM users WHERE id = 1;
+     *
+     *  // Check if deletion was successful
+     *  if (deleted) { System.out.println("User deleted successfully."); }
+     *  else { System.out.println("No user was deleted or user not found."); }
+     * */
+}
