4e71f1d8ecabfc4520ce32003d0b8f76a336a619
Keegan Pullen <126522882+FLUVIS@users.noreply.github.com>
2025-05-04 20:48:55 -0600

    Got the app icon and title working

--- src/main/java/com/example/app/emissionsApp.java
+++ src/main/java/com/example/app/emissionsApp.java
@@ -2,6 +2,7 @@ package com.example.app;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -11,6 +12,9 @@ public class emissionsApp extends javafx.application.Application{
     @Override
     public void start(Stage stage) throws IOException {
         try {
+            Image image = new Image(getClass().getResource("/images/app_logo.jpg").toExternalForm());
+            stage.getIcons().add(image);
+            stage.setTitle("WCU Emissions");
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root, 650, 400);
